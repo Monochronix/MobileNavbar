4a5b259d6d1c2681194797eb4841d5f434cfc26a
Monochronix <kylehuangmonochronix@gmail.com>
2024-10-31 21:02:53 -0700

    Animation doesn't play on page load

--- menuIconAnimation.js
+++ menuIconAnimation.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuBtn = document.querySelector('.menu-btn');
+    menuBtn.classList.add('firstLoad');
     const toggleMenu = () => {
         menuBtn.classList.toggle('open');
+        menuBtn.classList.remove('firstLoad');
     };
     menuBtn.addEventListener('click', toggleMenu);
     menuBtn.addEventListener('keydown', function(event) {
